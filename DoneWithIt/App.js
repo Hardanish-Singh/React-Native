46fd65873823e186d913494308ff7f21c4aac736
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2023-03-12 10:46:07 -0400

    DoneWithIt

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,6 +1,18 @@
 // import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+// import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { View } from "react-native";
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
-        return <ViewImageScreen />;
+        return (
+                <View
+                        style={{
+                                flex: 1,
+                                justifyContent: "center",
+                                alignItems: "center",
+                        }}
+                >
+                        <AppButton title="login" onPress={() => console.log("Button clicked")} />
+                </View>
+        );
 }
