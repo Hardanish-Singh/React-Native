eb50fad8059473b96e49b5d2b49d7f745ffb0161
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2023-11-04 13:13:44 -0400

    Code Formatting

--- TodoList/components/GoalInput.js
+++ TodoList/components/GoalInput.js
@@ -4,40 +4,37 @@ import { useState } from "react";
 import { TextInput, Button, StyleSheet, View } from "react-native";
 
 export default function GoalInput({ placeholder, addGoalHandler, title }) {
-        const [goalsText, setGoalsText] = useState("");
-        const goalInputHandler = (enteredText) => setGoalsText(enteredText);
+    const [goalsText, setGoalsText] = useState("");
+    const goalInputHandler = (enteredText) => setGoalsText(enteredText);
 
-        const handlePress = () => {
-                addGoalHandler(goalsText);
-                setGoalsText("");
-        };
+    const handlePress = () => {
+        addGoalHandler(goalsText);
+        setGoalsText("");
+    };
 
-        return (
-                <View style={styles.goalsInputContainer}>
-                        <TextInput
-                                style={styles.textInput}
-                                placeholder={placeholder}
-                                value={goalsText}
-                                onChangeText={goalInputHandler}
-                        />
-                        <Button
-                                title={title}
-                                onPress={handlePress}
-                        />
-                </View>
-        );
+    return (
+        <View style={styles.goalsInputContainer}>
+            <TextInput
+                style={styles.textInput}
+                placeholder={placeholder}
+                value={goalsText}
+                onChangeText={goalInputHandler}
+            />
+            <Button title={title} onPress={handlePress} />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-        goalsInputContainer: {
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-        },
-        textInput: {
-                borderWidth: 1,
-                width: "80%",
-                height: "100%",
-                marginLeft: 20,
-        },
+    goalsInputContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    textInput: {
+        borderWidth: 1,
+        width: "80%",
+        height: "100%",
+        marginLeft: 20,
+    },
 });
--- TodoList/components/GoalItem.js
+++ TodoList/components/GoalItem.js
@@ -2,30 +2,30 @@
 import { View, Text, StyleSheet, Pressable } from "react-native";
 
 export default function GoalItem({ text, deleteGoalHandler }) {
-        return (
-                <View style={styles.goalItemContainer}>
-                        <Pressable
-                                android_ripple={{ color: "#210644" }}
-                                onPress={deleteGoalHandler}
-                                style={({ pressed }) => pressed && styles.pressedItem}
-                        >
-                                <Text style={styles.goalItemText}>{text}</Text>
-                        </Pressable>
-                </View>
-        );
+    return (
+        <View style={styles.goalItemContainer}>
+            <Pressable
+                android_ripple={{ color: "#210644" }}
+                onPress={deleteGoalHandler}
+                style={({ pressed }) => pressed && styles.pressedItem}
+            >
+                <Text style={styles.goalItemText}>{text}</Text>
+            </Pressable>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-        goalItemContainer: {
-                margin: 10,
-                borderRadius: 10,
-                backgroundColor: "#5e0acc",
-        },
-        goalItemText: {
-                color: "white",
-                padding: 10,
-        },
-        pressedItem: {
-                opacity: 0.5,
-        },
+    goalItemContainer: {
+        margin: 10,
+        borderRadius: 10,
+        backgroundColor: "#5e0acc",
+    },
+    goalItemText: {
+        color: "white",
+        padding: 10,
+    },
+    pressedItem: {
+        opacity: 0.5,
+    },
 });
