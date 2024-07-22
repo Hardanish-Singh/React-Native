a231546a1c39fa50a777dcc80397cfe6451c0e2c
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-22 19:13:28 -0400

    Aora React Native App

--- aora/app/(tabs)/_layout.tsx
+++ aora/app/(tabs)/_layout.tsx
@@ -2,7 +2,14 @@ import { Tabs } from "expo-router";
 import { Image, Text, View } from "react-native";
 import { icons } from "../../constants";
 
-const TabIcon = ({ icon, color, name, focused }: any) => {
+type TabIconProps = {
+    icon: any;
+    color: string;
+    name: string;
+    focused: boolean;
+};
+
+const TabIcon = ({ icon, color, name, focused }: TabIconProps) => {
     return (
         <View className="flex items-center justify-center gap-2">
             <Image source={icon} resizeMode="contain" tintColor={color} className="w-6 h-6" />
--- aora/app/index.tsx
+++ aora/app/index.tsx
@@ -1,3 +1,5 @@
+import CustomButton from "@/components/CustomButton";
+import { router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -29,6 +31,11 @@ export default function App() {
                     <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
                         Where Creativity Meets Innovation: Embark on a Journey of Limitless Exploration with Aora
                     </Text>
+                    <CustomButton
+                        title="Continue with Email"
+                        handlePress={() => router.push("/sign-in")}
+                        containerStyles="w-full mt-7"
+                    />
                 </View>
             </ScrollView>
             <StatusBar backgroundColor="#161622" style="light" />
