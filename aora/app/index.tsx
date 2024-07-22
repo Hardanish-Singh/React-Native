1ded5a8e23e89791329b679cd44735fa4b7bc4c3
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-22 17:18:49 -0400

    Aora React Native App

--- aora/app/index.tsx
+++ aora/app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
@@ -6,6 +7,9 @@ export default function App() {
         <View className="flex-1 items-center justify-center bg-white">
             <Text className="text-3xl font-pblack">Aora!</Text>
             <StatusBar style="auto" />
+            <Link href="/home" style={{ color: "blue" }}>
+                Go to Home
+            </Link>
         </View>
     );
 }
