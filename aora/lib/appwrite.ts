5ea4f4659836647b58f2e02177f34f2a65ab8f4d
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-24 19:27:57 -0400

    Aora React Native App

---
+++ aora/lib/appwrite.ts
@@ -0,0 +1,8 @@
+export const appwriteConfig = {
+    endpoint: "https://cloud.appwrite.io/v1",
+    platform: "com.jsm.sora", // BundleId
+    projectId: "66a0ef3f00197b533375",
+    databaseId: "66a0f0ac0023fb75d512",
+    userCollectionId: "66a0f0cc0004220d7044",
+    videoCollectionId: "66a0f0fd0034224014bb",
+};
