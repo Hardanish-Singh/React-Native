b7e20011ad01997f4710f990412a5953a3a37a85
Hardanish Singh <hardanishsingh1@gmail.com>
2024-07-31 18:23:10 -0400

    Aora React Native App

--- aora/app/(tabs)/home.tsx
+++ aora/app/(tabs)/home.tsx
@@ -1,9 +1,30 @@
-import { Text, View } from "react-native";
+import { FlatList, Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../../constants";
 
 export default function Home() {
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView className="bg-primary">
+            <FlatList
+                data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+                keyExtractor={(item: any) => item.id}
+                renderItem={({ item }) => <Text className="text-3xl text-white">{item.id}</Text>}
+                ListHeaderComponent={() => (
+                    <View className="flex my-6 px-4 space-y-4">
+                        <View className="flex justify-between items-start flex-row mb-6">
+                            <View>
+                                <Text className="font-pmedium text-sm text-gray-100">Welcome Back</Text>
+                                <Text className="text-2xl font-psemibold text-white">JSMastery</Text>
+                            </View>
+
+                            <View className="mt-1.5">
+                                <Image source={images.logoSmall} className="w-9 h-10" resizeMode="contain" />
+                            </View>
+                        </View>
+                    </View>
+                )}
+            />
+        </SafeAreaView>
     );
 }
