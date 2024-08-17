ed87eed5edf34bd40e91d64d6ef4699c385b09a5
Hardanish Singh <hardanishsingh1@gmail.com>
2024-08-17 18:07:48 -0400

    Aora React Native App

--- aora/app/(tabs)/create.tsx
+++ aora/app/(tabs)/create.tsx
@@ -1,10 +1,31 @@
-import { Text } from "react-native";
+import FormField from "@/components/FormField";
+import { useState } from "react";
+import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Create() {
+    const [uploading, setUploading] = useState(false);
+    const [form, setForm] = useState({
+        title: "",
+        video: null,
+        thumbnail: null,
+        prompt: "",
+    });
     return (
         <SafeAreaView className="bg-primary h-full">
-            <Text>Create</Text>
+            <ScrollView className="px-4 my-6">
+                <Text className="text-2xl text-white font-psemibold">Upload Video</Text>
+                <FormField
+                    title="Video Title"
+                    value={form.title}
+                    placeholder="Give your video a catchy title..."
+                    handleChangeText={(e) => setForm({ ...form, title: e })}
+                    otherStyles="mt-10"
+                />
+                <View className="mt-7 space-y-2">
+                    <Text className="text-base text-gray-100 font-pmedium">Upload Video</Text>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     );
 }
