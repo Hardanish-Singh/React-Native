02beb67b725fced8c38e0643264a7232537ba6bd
Hardanish Singh <hardanishsingh1@gmail.com>
2024-09-04 20:24:10 -0400

    Aora React Native App

--- aora/components/FormField.tsx
+++ aora/components/FormField.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Image, Text, TextInput, TextInputProps, TouchableOpacity, View } from "react-native";
-
 import { icons } from "../constants";
 
 type FormFieldProps = {
