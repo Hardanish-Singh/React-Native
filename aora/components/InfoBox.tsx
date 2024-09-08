fccf59617e60fce76a788dbe8549c50c8409c3bb
Hardanish Singh <hardanishsingh1@gmail.com>
2024-09-08 18:13:19 -0400

    Aora React Native App

--- aora/components/InfoBox.tsx
+++ aora/components/InfoBox.tsx
@@ -7,13 +7,11 @@ type InfoBoxProps = {
     titleStyles?: string;
 };
 
-const InfoBox = ({ title, subtitle, containerStyles, titleStyles }: InfoBoxProps) => {
-    return (
-        <View className={containerStyles}>
-            <Text className={`text-white text-center font-psemibold ${titleStyles}`}>{title}</Text>
-            <Text className="text-sm text-gray-100 text-center font-pregular">{subtitle}</Text>
-        </View>
-    );
-};
+const InfoBox: React.FC<InfoBoxProps> = ({ title, subtitle, containerStyles, titleStyles }: InfoBoxProps) => (
+    <View className={containerStyles}>
+        <Text className={`text-white text-center font-psemibold ${titleStyles}`}>{title}</Text>
+        <Text className="text-sm text-gray-100 text-center font-pregular">{subtitle}</Text>
+    </View>
+);
 
 export default InfoBox;
