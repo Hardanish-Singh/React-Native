3257f4aa2c32ac35f3dfc94034181d9fae14a404
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-09-17 16:26:37 -0400

    Aora React Native App

--- aora/app/(auth)/_layout.tsx
+++ aora/app/(auth)/_layout.tsx
@@ -1,26 +1,24 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
-const AuthLayout: React.FC = (): React.JSX.Element => {
-    return (
-        <>
-            <Stack>
-                <Stack.Screen
-                    name="sign-in"
-                    options={{
-                        headerShown: false,
-                    }}
-                />
-                <Stack.Screen
-                    name="sign-up"
-                    options={{
-                        headerShown: false,
-                    }}
-                />
-            </Stack>
-            <StatusBar backgroundColor="#161622" style="light" />
-        </>
-    );
-};
+const AuthLayout: React.FC = (): React.JSX.Element => (
+    <>
+        <Stack>
+            <Stack.Screen
+                name="sign-in"
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <Stack.Screen
+                name="sign-up"
+                options={{
+                    headerShown: false,
+                }}
+            />
+        </Stack>
+        <StatusBar backgroundColor="#161622" style="light" />
+    </>
+);
 
 export default AuthLayout;
