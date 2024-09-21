ec971302f320386a178f180efa0b3e5827ec8278
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-09-21 09:36:54 -0400

    Aora React Native App

--- aora/app/(auth)/sign-up.tsx
+++ aora/app/(auth)/sign-up.tsx
@@ -49,16 +49,13 @@ const SignUp: React.FC = (): React.JSX.Element => {
                         }}
                     >
                         <Image source={images.logo} resizeMode="contain" className="w-[115px] h-[34px]" />
-
                         <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">Sign Up to Aora</Text>
-
                         <FormField
                             title="Username"
                             value={form.username}
                             handleChangeText={(e) => handleChange("username", e)}
                             otherStyles="mt-10"
                         />
-
                         <FormField
                             title="Email"
                             value={form.email}
@@ -66,21 +63,18 @@ const SignUp: React.FC = (): React.JSX.Element => {
                             otherStyles="mt-7"
                             keyboardType="email-address"
                         />
-
                         <FormField
                             title="Password"
                             value={form.password}
                             handleChangeText={(e) => handleChange("password", e)}
                             otherStyles="mt-7"
                         />
-
                         <CustomButton
                             title="Sign Up"
                             handlePress={submit}
                             containerStyles="mt-7"
                             isLoading={isSubmitting}
                         />
-
                         <View className="flex justify-center pt-5 flex-row gap-2">
                             <Text className="text-lg text-gray-100 font-pregular">Have an account already?</Text>
                             <Link href="/sign-in" className="text-lg font-psemibold text-secondary">
