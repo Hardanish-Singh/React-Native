24227f1d2384d0984dc6e8fb2d78dd129e0014f3
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-09-29 14:32:07 -0400

    Aora React Native App

--- aora/app/search/[query].tsx
+++ aora/app/search/[query].tsx
@@ -1,15 +1,14 @@
+import EmptyState from "@/components/EmptyState";
+import SearchInput from "@/components/SearchInput";
+import VideoCard from "@/components/VideoCard";
 import { useLocalSearchParams } from "expo-router";
 import { useEffect } from "react";
 import { FlatList, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
-import EmptyState from "@/components/EmptyState";
-import SearchInput from "@/components/SearchInput";
-import VideoCard from "@/components/VideoCard";
 import { searchPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 
-const Search = () => {
+const Search: React.FC = (): React.JSX.Element => {
     const { query } = useLocalSearchParams<any>();
     const { data: posts, refetch } = useAppwrite(() => searchPosts(query));
 
@@ -24,15 +23,13 @@ const Search = () => {
                 keyExtractor={(item: any) => item.$id}
                 renderItem={({ item }) => <VideoCard video={item} />}
                 ListHeaderComponent={() => (
-                    <>
-                        <View className="flex my-6 px-4">
-                            <Text className="font-pmedium text-gray-100 text-sm">Search Results</Text>
-                            <Text className="text-2xl font-psemibold text-white mt-1">{query}</Text>
-                            <View className="mt-6 mb-8">
-                                <SearchInput initialQuery={query} refetch={refetch} />
-                            </View>
+                    <View className="flex my-6 px-4">
+                        <Text className="font-pmedium text-gray-100 text-sm">Search Results</Text>
+                        <Text className="text-2xl font-psemibold text-white mt-1">{query}</Text>
+                        <View className="mt-6 mb-8">
+                            <SearchInput initialQuery={query} refetch={refetch} />
                         </View>
-                    </>
+                    </View>
                 )}
                 ListEmptyComponent={() => (
                     <EmptyState title="No Videos Found" subtitle="No videos found for this search query" />
