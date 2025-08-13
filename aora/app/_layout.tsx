2f0e1b726933ee55ef67f0692729738dbab22b5b
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-08-13 14:32:06 -0400

    Aora React Native App

--- aora/app/_layout.tsx
+++ aora/app/_layout.tsx
@@ -4,7 +4,7 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
+// Prevent the splash screen from auto-hiding before asset loading is complete
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout: React.FC = (): React.JSX.Element | null => {
