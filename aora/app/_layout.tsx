5a5d68d160521a7e916de24d53720d0f4e468f55
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-08-14 17:14:00 -0400

    Aora React Native App

--- aora/app/_layout.tsx
+++ aora/app/_layout.tsx
@@ -8,7 +8,7 @@ import { useEffect } from "react";
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout: React.FC = (): React.JSX.Element | null => {
-    // Load the custom fonts before the app loads.
+    // Load the custom fonts before the app loads
     const [fontsLoaded, error] = useFonts({
         "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
         "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
